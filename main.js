43801fb4cefcc9210dccdcce8f6a9385fc7315b9
Miranda Stopa <miranda.stopa@gmail.com>
2021-09-13 10:40:23 -0400

    heroku debugging

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const { db } = require("./server/db");
+const app = require("./server");
+const PORT = process.env.PORT || 3000;
+
+db.sync().then(() => {
+  console.log("db synced");
+  app.listen(PORT, () =>
+    console.log(`studiously serving silly sounds on port ${PORT}`)
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: __dirname,
     filename: "./public/bundle.js",
   },
-  // mode: 'development',
+  mode: "development",
   // context: __dirname,
   devtool: "source-map",
   // resolve: {
